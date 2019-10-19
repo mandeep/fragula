667ec3e2dd5def6811a9ed4850163d6f00f14f27
mandeep <mandeep@users.noreply.github.com>
2019-10-18 21:35:37 -0400

    Format code with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use luminance::context::GraphicsContext;
 use luminance_glfw::{Action, GlfwSurface, Key, Surface, WindowDim, WindowEvent, WindowOpt};
 
 fn main() {
-    let surface = GlfwSurface::new(WindowDim::Windowed(1200, 900), "Fragula", WindowOpt::default());
+    let surface = GlfwSurface::new(WindowDim::Windowed(1200, 900),
+                                   "Fragula",
+                                   WindowOpt::default());
 
     if let Ok(surface) = surface {
         event_loop(surface);
@@ -19,14 +21,17 @@ fn event_loop(mut surface: GlfwSurface) {
     'run: loop {
         for event in surface.poll_events() {
             match event {
-                WindowEvent::Close | WindowEvent::Key(Key::Escape, _, Action::Release, _) => break 'run,
+                WindowEvent::Close | WindowEvent::Key(Key::Escape, _, Action::Release, _) => {
+                    break 'run
+                }
                 _ => (),
             }
         }
 
         let color = [0.122, 0.173, 0.227, 1.0];
 
-        surface.pipeline_builder().pipeline(&back_buffer, color, |_, _| ());
+        surface.pipeline_builder()
+               .pipeline(&back_buffer, color, |_, _| ());
         surface.swap_buffers();
     }
 }
