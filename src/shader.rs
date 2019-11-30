858de9c8efcbd3b548688ab7bc2ede266086e6e4
mandeep <mandeep@users.noreply.github.com>
2019-11-29 21:05:24 -0500

    Allow unbound uniform for all uniforms

--- src/shader.rs
+++ src/shader.rs
@@ -6,7 +6,10 @@ use luminance_derive::UniformInterface;
 pub struct ShaderInterface {
     #[uniform(unbound)]
     pub projection: Uniform<M44>,
+    #[uniform(unbound)]
     pub view: Uniform<M44>,
+    #[uniform(unbound)]
     pub rotation: Uniform<M44>,
+    #[uniform(unbound)]
     pub translation: Uniform<M44>,
 }
