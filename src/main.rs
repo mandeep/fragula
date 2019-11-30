823e1763609fc93a22f0f502bc3a5f130bfd84de
mandeep <mandeep@users.noreply.github.com>
2019-11-29 20:24:50 -0500

    Translate using translation matrix instead of view matrix

--- src/main.rs
+++ src/main.rs
@@ -30,13 +30,16 @@ fn render_loop(mut surface: GlfwSurface) {
                                  z_near,
                                  z_far);
 
-    let mut eye = Point3::new(0.0, 0.5, 4.0);
+    let eye = Point3::new(0.0, 0.5, 4.0);
     let center = Point3::origin();
     let up = Vector3::unit_y();
-    let mut view = Matrix4::<f32>::look_at(eye, center, up);
+    let view = Matrix4::<f32>::look_at(eye, center, up);
 
     let (mut x_angle, mut y_angle, mut z_angle) = (1.0, 1.0, 1.0);
     let mut rotation: Matrix4<f32> = SquareMatrix::identity();
+
+    let mut xyz_axis = Vector3::new(0.0, 0.0, 0.0);
+    let mut translation: Matrix4<f32> = SquareMatrix::identity();
 
     let mesh = Obj::load(path).unwrap().to_tess(&mut surface).unwrap();
 
@@ -93,13 +96,13 @@ fn render_loop(mut surface: GlfwSurface) {
                 }
                 WindowEvent::Key(Key::Z, _, Action::Release, _)
                 | WindowEvent::Key(Key::Z, _, Action::Repeat, _) => {
-                    eye.z += 0.1;
-                    view = Matrix4::<f32>::look_at(eye, center, up);
+                    xyz_axis.z -= 0.1;
+                    translation = Matrix4::from_translation(xyz_axis);
                 }
                 WindowEvent::Key(Key::X, _, Action::Release, _)
                 | WindowEvent::Key(Key::X, _, Action::Repeat, _) => {
-                    eye.z -= 0.1;
-                    view = Matrix4::<f32>::look_at(eye, center, up);
+                    xyz_axis.z += 0.1;
+                    translation = Matrix4::from_translation(xyz_axis);
                 }
                 _ => (),
             }
@@ -112,6 +115,7 @@ fn render_loop(mut surface: GlfwSurface) {
                    shd_gate.shade(&program, |interface, mut rdr_gate| {
                                interface.projection.update(projection.into());
                                interface.view.update(view.into());
+                               interface.translation.update(translation.into());
                                interface.rotation.update(rotation.into());
 
                                rdr_gate.render(RenderState::default(), |mut tess_gate| {
