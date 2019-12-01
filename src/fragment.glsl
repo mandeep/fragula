83c524948bb08899b58f0a4e46b2523d4b8980da
mandeep <mandeep@users.noreply.github.com>
2019-12-01 00:41:41 -0500

    Use max between dot product and 0.0

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -5,7 +5,7 @@ out vec3 frag_color;
 void main() {
     vec3 object_color = vec3(0.6, 0.6, 0.6);
     vec3 light_direction = vec3(0.0, -1.0, -0.5);
-    float kd = dot(vertex_normal, -light_direction);
+    float kd = max(dot(vertex_normal, -light_direction), 0.0);
 
     frag_color = kd * object_color;
 }
