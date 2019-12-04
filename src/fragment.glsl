74c7159b531564090e36d97738b83b4e949513b4
mandeep <mandeep@users.noreply.github.com>
2019-12-03 20:15:26 -0500

    Change light vector in z-direction back to -0.5
    This reverts commit 06f4326079f6979d42c1bc5c8096eaa21290fe26.

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -8,7 +8,7 @@ out vec3 frag_color;
 
 void main() {
     vec3 object_color = vec3(0.6, 0.6, 0.6);
-    vec3 light_direction = vec3(0.0, -1.5, -1.0);
+    vec3 light_direction = vec3(0.0, -1.0, -0.5);
     float kd = max(dot(vertex_normal, -light_direction), 0.0);
 
     frag_color = kd * object_color;
