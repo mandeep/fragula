14db2b5a5a78d7e43de7702797ad0d2b971ae6c2
mandeep <mandeep@users.noreply.github.com>
2019-12-24 01:58:47 -0500

    Replace lambertian with oren-nayar

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -6,10 +6,27 @@ in vec3 vertex_normal;
 
 out vec3 frag_color;
 
-void main() {
-    vec3 object_color = vec3(0.6, 0.6, 0.6);
-    vec3 light_direction = vec3(0.0, 1.0, 0.5);
-    float kd = max(dot(vertex_normal, light_direction), 0.0);
+const float PI = 3.14159265359;
 
-    frag_color = kd * object_color;
+void main() {
+	float sigma = 0.8;
+	float alpha = 1.0 / (PI + (PI/2.0 - 2.0/3.0) * sigma);
+	float beta = sigma / (PI + (PI/2.0 - 2.0/3.0) * sigma);
+
+    vec3 light_direction = vec3(0.0, 1.5, 0.75);
+    vec3 view_direction = vec3(0.0, 5.0, 5.0);
+	vec3 albedo = vec3(1.0, 1.0, 1.0);
+
+	float nl = max(dot(vertex_normal, light_direction), 0.0);
+	float nv = max(dot(vertex_normal, view_direction), 0.0);
+	float lv = dot(light_direction, view_direction);
+
+	float s = lv - nl * nv;
+	float t = 1.0;
+
+	if (s > 0.0) {
+		t = max(nl, nv);
+	}
+
+    frag_color = albedo * nl * (alpha + (beta * (s / t)));
 }
