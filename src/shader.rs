1cb53d288a4f1a126748149cd58cde09ae4a125f
mandeep <mandeep@users.noreply.github.com>
2020-01-08 22:52:41 -0600

    Add functions for creation of fragment shader and shader program

--- src/shader.rs
+++ src/shader.rs
@@ -1,6 +1,12 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 use luminance::linear::M44;
-use luminance::shader::program::Uniform;
+use luminance::shader::program::{Program, Uniform};
 use luminance_derive::UniformInterface;
+
+use crate::vertex::VertexSemantics;
+
 
 #[derive(Debug, UniformInterface)]
 pub struct ShaderInterface {
@@ -12,4 +18,22 @@ pub struct ShaderInterface {
     pub rotation: Uniform<M44>,
     #[uniform(unbound)]
     pub translation: Uniform<M44>,
+}
+
+
+pub fn create_fragment_shader(file: &String) -> String {
+    let mut fragment_file = File::open(file).unwrap();
+    let mut fragment_shader = String::new();
+    fragment_file.read_to_string(&mut fragment_shader).unwrap();
+
+    fragment_shader
+}
+
+
+pub fn create_shader_program(vertex_shader: &String, fragment_shader: &String) -> Program<VertexSemantics, (), ShaderInterface> {
+    let program: Program<VertexSemantics, (), ShaderInterface> =
+        Program::from_strings(None, vertex_shader, None, &fragment_shader).unwrap()
+                                                                          .ignore_warnings();
+
+    program
 }
