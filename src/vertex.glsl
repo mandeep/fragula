5168645b768854bb3e7d583dac7ef49e9b24aed3
mandeep <mandeep@users.noreply.github.com>
2020-03-06 18:58:03 -0500

    Output texture vertex from vertex shader to fragment shader

--- src/vertex.glsl
+++ src/vertex.glsl
@@ -1,4 +1,5 @@
 in vec3 position;
+in vec3 texture;
 in vec3 normal;
 
 uniform mat4 projection;
@@ -7,8 +8,10 @@ uniform mat4 rotation;
 uniform mat4 translation;
 
 out vec3 vertex_normal;
+out vec3 texture_coordinate;
 
 void main() {
     vertex_normal = normal;
+    texture_coordinate = texture;
     gl_Position = projection * view * translation * rotation * vec4(position, 1.0);
 }
