19c001c7fd039ded96649448eee667a05eacce02
mandeep <mandeep@users.noreply.github.com>
2020-03-09 00:32:20 -0400

    Create resolution array and pass to render loop for the uniform variable

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ use luminance_glfw::{GlfwSurface, Surface, WindowDim, WindowOpt};
 use crate::render::render_loop;
 
 fn main() {
+    let resolution = [1200, 900];
+
     let obj_path = env::args().skip(1)
                               .next()
                               .expect("Error: Invalid OBJ file path.");
@@ -20,12 +22,12 @@ fn main() {
                                    .next()
                                    .expect("Error: Invalid fragment shader file path.");
 
-    let surface = GlfwSurface::new(WindowDim::Windowed(1200, 900),
+    let surface = GlfwSurface::new(WindowDim::Windowed(resolution[0], resolution[1]),
                                    "Fragula",
                                    WindowOpt::default());
 
     if let Ok(surface) = surface {
-        render_loop(surface, obj_path, fragment_path);
+        render_loop(surface, obj_path, fragment_path, resolution);
     } else {
         panic!("Could not create surface.");
     }
