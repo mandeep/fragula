a9f75a3e58daf361abc42ad191e043bb21fda763
mandeep <mandeep@users.noreply.github.com>
2020-03-24 23:01:41 -0400

    Add texture image as argument

--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,27 @@ fn main() {
     let matches = App::new("Fragula")
         .version("0.6.0")
         .author("Mandeep <github.com/mandeep>")
-        .arg(Arg::with_name("obj_file")
+        .arg(Arg::with_name("obj")
             .help("Obj file to load.")
             .required(true)
             .index(1)
             )
-        .arg(Arg::with_name("fragment_shader")
+        .arg(Arg::with_name("shader")
             .help("Fragment shader to load.")
             .required(true)
             .index(2)
             )
+        .arg(Arg::with_name("texture")
+            .help("Texture image to load.")
+            .short("t")
+            .long("texture")
+            .takes_value(true)
+            )
         .get_matches();
 
 
-    let obj = matches.value_of("obj_file").unwrap();
-    let fragment = matches.value_of("fragment_shader").unwrap();
+    let obj = matches.value_of("obj").unwrap();
+    let fragment = matches.value_of("shader").unwrap();
 
 
     let obj_path = Path::new(&obj);
