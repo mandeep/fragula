55bdda92462781d0ce643ec67e5f0abbbcd265e3
mandeep <mandeep@users.noreply.github.com>
2020-03-25 20:22:01 -0400

    Add load_image function to read image to disk

--- src/texture.rs
+++ src/texture.rs
@@ -1,7 +1,20 @@
 use std::path::Path;
 
 use image::RgbImage;
+use luminance::pixel::NormRGB8UI;
+use luminance::texture::{Dim2, GenMipmaps, Sampler, Texture};
+use luminance_glfw::GlfwSurface;
 
 pub fn read_image(path: &Path) -> Option<RgbImage> {
     image::open(path).map(|img| img.flipv().to_rgb()).ok()
+}
+
+pub fn load_image(surface: &mut GlfwSurface, img: RgbImage) -> Texture<Dim2, NormRGB8UI> {
+    let (width, height) = img.dimensions();
+    let texels = img.into_raw();
+
+    let tex = Texture::new(surface, [width, height], 0, Sampler::default()).unwrap();
+    tex.upload_raw(GenMipmaps::No, &texels).unwrap();
+
+    tex
 }
