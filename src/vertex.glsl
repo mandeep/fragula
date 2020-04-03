62233766de3f9e0d0fe2011647f2d2203ee4a81a
mandeep <mandeep@users.noreply.github.com>
2020-04-03 00:15:44 -0400

    Add scale uniform variable to model view matrix

--- src/vertex.glsl
+++ src/vertex.glsl
@@ -6,12 +6,13 @@ uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 rotation;
 uniform mat4 translation;
+uniform mat4 scale;
 
 out vec3 vertex_normal;
 out vec3 texture_coordinate;
 
 void main() {
-    mat4 model_view = view * translation * rotation;
+    mat4 model_view = view * translation * rotation * scale;
     vertex_normal = normalize(model_view * vec4(normal, 0.0)).xyz;
     texture_coordinate = texture;
     gl_Position = projection * model_view * vec4(position, 1.0);
